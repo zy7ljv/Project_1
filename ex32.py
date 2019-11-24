af0843d338e1dc6b413d1b87278885c3f795f1f0
zy7ljv <zy7ljv@gmail.com>
2019-11-24 13:20:46 -0500

    ex33

--- ex32.py
+++ ex32.py
@@ -3,8 +3,10 @@ fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
 # this first kind of for-loop goes through a list
-for number in  the_count:
+for number in the_count:
     print(f"This is count {number}")
+
+print(">>>>>>>> the_count", repr(the_count))
 
 # same as above
 for fruit in fruits:
@@ -16,13 +18,18 @@ for i in change:
     print(f"I got {i}")
 
 # we can also build lists, first start with an empty one
-elements = []
+elements = ['a', 'b']
 
 # then use the range function to do 0 to 5 counts
 for i in range(0, 6):
     print(f"Adding {i} to the list.")
+
+    print(">>>>> elements", repr(elements))
+
     # append is a function that lists understand
     elements.append(i)
+
+print(">>>>> elements", repr(elements))
 
 # now we can print them out too
 for i in elements:
