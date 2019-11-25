a8d8b6159de5ab7bd6e671e6eaa653eda24e4ea9
zy7ljv <zy7ljv@gmail.com>
2019-11-24 20:07:35 -0500

    ex35

---
+++ ex34.py
@@ -0,0 +1,21 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+
+# The third(3rd) animal.
+
+# The first(1st) animal.
+
+# The animal at 3
+
+# The fifth(5th) animal
+
+# The animal at 2
+
+# The sixth(6th) animal
+
+# The animal at 4.
+
+print(animals[0])
+print(animals[1:4])
+print(animals[:4])
