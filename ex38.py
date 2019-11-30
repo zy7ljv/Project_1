6bea4b8a41a5958e78eac89e7514b23323b560a3
zy7ljv <zy7ljv@gmail.com>
2019-11-29 23:01:12 -0500

    update to ex40

---
+++ ex38.py
@@ -0,0 +1,26 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait, there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee",
+              "Corn", "Banana", "Girl", "Boy"]
+
+i = len(stuff)
+print(">>>>> i=", i)
+for i in range(6, 10):
+    next_one = more_stuff.pop()
+    print("Adding", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+    print("<<<<< i =", i)
+
+print("There we go:", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
