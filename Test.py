3392da75276801d82919304c29168618e54ff075
zy7ljv <zy7ljv@gmail.com>
2019-12-05 17:31:48 -0500

    Still working on ex42

---
+++ Test.py
@@ -0,0 +1,16 @@
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
+
+    def addtwice(self, x):
+        self.add(x)
+        self.add(x)
+
+        a = Bag.addtwice()
+
+
+
+        print(">>>", a)
